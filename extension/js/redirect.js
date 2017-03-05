b6bdbbeacc60c6939978edbf0a017f9dad5672b5
David Cincotta <decincotta@verizon.net>
2017-03-05 14:19:43 -0500

    html change

--- extension/js/redirect.js
+++ extension/js/redirect.js
@@ -1,10 +1,9 @@
+
 var userNumber = 1;
 var toUrl = "https://classroom.google.com/u/"+userNumber+"/h";
 // window.open(", "_self");
-
 console.log(location.toString());
 console.log('tourl: ' + toUrl);
-
 if (!location.toString().includes(toUrl)) {
 	location.assign(toUrl);
 	console.log('tru')
